1125c2d41fd50b8ce2866a995c0d8d9bfe1c0455
Sumedha Shreya <sumedhashreya04@gmail.com>
2026-05-15 20:12:44 +0530

    Create shutillib.py

---
+++ learningphase/shutillib.py
@@ -0,0 +1,17 @@
+#FOR PRACTICE
+import shutil
+import os
+os.chdir("Users/sumedhashreya/Desktop/Coding/Python")
+def ignorereturnlist(directory,files):
+  return [f for f in files if f=="example.txt"]
+shutil.copytree("deleteit","deletit2",ignore=ignorereturnlist)  
+""" 
+"""
+os.chdir("Users/sumedhashreya/Desktop/Coding/Python")
+print(os.getcwd())
+os.mkdir("deleteit")
+path=os.path.abspath("deleteit")
+os.chdir(path)
+with open("example.txt",'w') as f:
+    f.write("Hello World")
+shutil.copy("example.txt","example2.txt")

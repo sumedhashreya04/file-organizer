ce6985552e730c19f2f36882709c898cca7ee826
Sumedha Shreya <sumedhashreya04@gmail.com>
2026-05-15 18:38:27 +0530

    Create categories.py

---
+++ organizer/categories.py
@@ -0,0 +1,8 @@
+IMAGE_EXTENSIONS=[".jpg",".png",".jpeg",".gif"]
+VIDEO_EXTENSIONS=[".mp4",".mkv"]
+TEXT_EXTENSIONS=[".docx",".txt",".csv"]
+COMPRESS_EXTENSIONS=[".zip"]
+DEVELEPOR_EXTENSIONS=[".html",".htm",".css",".cpp",".java",".py",".c",".js",".class",".php"]
+"""
+
+"""
